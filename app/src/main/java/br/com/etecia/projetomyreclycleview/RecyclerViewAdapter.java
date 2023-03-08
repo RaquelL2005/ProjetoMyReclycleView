7f77816e6999c754c66edf47748d838a299c9bb3
Raquell2005 <raqdezotti@gmail.com>
2023-03-07 21:57:58 -0300

    "qualquer coisa"

---
+++ app/src/main/java/br/com/etecia/projetomyreclycleview/RecyclerViewAdapter.java
@@ -0,0 +1,7 @@
+package br.com.etecia.projetomyreclycleview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+}
