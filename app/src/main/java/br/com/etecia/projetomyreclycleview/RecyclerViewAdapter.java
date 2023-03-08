c4d9f903a5cde40c6c62f9c459ede8fdbbc6b14d
Raquell2005 <raqdezotti@gmail.com>
2023-03-07 22:05:46 -0300

    "qualquer coisa"

--- app/src/main/java/br/com/etecia/projetomyreclycleview/RecyclerViewAdapter.java
+++ app/src/main/java/br/com/etecia/projetomyreclycleview/RecyclerViewAdapter.java
@@ -1,7 +1,36 @@
 package br.com.etecia.projetomyreclycleview;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    private Context mContext;
+    private List<Filmes> mData;
 
+
+    public RecyclerViewAdapter(Context mContext, List<Filmes> mData) {
+        this.mContext = mContext;
+        this.mData = mData;}
+
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+         View view;
+        LayoutInflater mInflater
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
+
